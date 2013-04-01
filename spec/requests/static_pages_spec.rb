67fc4495187a4369b6044fb69492118958f3d4db
katiefritts <kathryn.a.fritts@gmail.com>
2013-04-01 23:05:53 +0200

    changed routes and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,9 +5,32 @@ describe "StaticPages" do
   describe "Home page" do
    
     it "should have the content 'Template App'" do
-    visit '/static_pages/home'
+    visit root_path
     page.should have_content('Template App')
 
     end
   end
+  describe "Help page" do
+   
+    it "should have the content 'Help'" do
+    visit help_path
+    page.should have_content('Help')
+	end
+	end
+
+	describe "About page" do
+   
+    it "should have the content 'About'" do
+    visit about_path
+    page.should have_content('About')
+   	end
+	end
+
+	describe "Contact Us" do
+   
+    it "should have the content 'Contact Us'" do
+    visit contact_path
+    page.should have_content('Contact Us')
+   	end
+	end
 end
